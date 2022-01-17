3fc7688e089026726bf2d1e9589c36ba1f82695b
Nguyen Xuan Duc <ducnx4@gmail.com>
2022-01-17 13:00:36 +0700

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', function(re,res) {
   res.send('Hello ducnx4 ')
 });
 
-var port = process.argv[2] || 3000;
+var port = process.argv[2] || 80;
 
 app.listen(port);
 console.log('app listening on port' + port)
